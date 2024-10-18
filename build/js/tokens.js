1678092b831b74b635a926b7aa56664e7e772741
BrunoCaputo <brunopc.dev@gmail.com>
2024-10-18 16:36:00 -0300

    First commit

---
+++ build/js/tokens.js
@@ -0,0 +1,12 @@
+/**
+ * Do not edit directly, this file was auto-generated.
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#27272a",
+  "ColorGray900": "#09090b",
+  "ColorPurple500": "#8b5cf6"
+};
